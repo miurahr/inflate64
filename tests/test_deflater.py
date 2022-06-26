90dd309ab949a06307aaa55580a1d38956cceabe
Hiroshi Miura <miurahr@linux.com>
2022-06-27 07:38:02 +0900

    test: change deflater test case
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_deflater.py
+++ tests/test_deflater.py
@@ -5,14 +5,15 @@ import inflate64
 
 BLOCKSIZE = 8192
 testdata_path = pathlib.Path(os.path.dirname(__file__)).joinpath("data")
-
+testdata = testdata_path.joinpath("test-file.bin")
 
 def test_deflater():
     deflater = inflate64.Deflater()
-    with testdata_path.joinpath("test-file.10").open("rb") as f:
+    with testdata.open("rb") as f:
         data = f.read(BLOCKSIZE)
         while len(data) > 0:
             compressed = deflater.deflate(data)
             data = f.read(BLOCKSIZE)
         compressed += deflater.flush()
         assert len(compressed) > 3000
+        assert len(compressed) < 4000
